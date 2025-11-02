791a3bdb5d2e9f31a5083f6e927ea92fb9788b43
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-02 21:15:42 +0100

    [CHANGED] Adaptation en cas de possession d'une playlist partagée

--- Deefy/src/Classes/action/DisplaySavePlaylist.php
+++ Deefy/src/Classes/action/DisplaySavePlaylist.php
@@ -30,13 +30,26 @@ class DisplaySavePlaylist extends Action
             }
             $rep = DeefyRepository::getInstance();
             $playlists = $rep->findAllUserPlaylists($user['id'], $user['email']);
-            $res="<h2>User Playlist</h2><nava>";
-            foreach($playlists as $playlist){
-                if(!empty($_POST['playlist']))
-                    if($playlist->__get('nom') == $_POST['playlist']){
-                        $_SESSION['playlist'] = $playlist;
-                        return $this->form()."<h2>Playlist selected!!</h2>";
-                    }
+            if(!empty($playlists)){
+                $res="<h2>User Playlist</h2><nava>";
+                foreach($playlists as $playlist){
+                    if(!empty($_POST['playlist']))
+                        if($playlist->__get('nom') == $_POST['playlist']){
+                            $_SESSION['playlist'] = $playlist;
+                            return $this->form()."<h2>Playlist selected!!</h2>";
+                        }
+                }
+            }
+            $playlists = $rep->findAllPlaylistShared($user['id'], $user['email']);
+            if(!empty($playlists)){
+                $res="<h2>User shared Playlist</h2><nava>";
+                foreach($playlists as $playlist){
+                    if(!empty($_POST['playlist']))
+                        if($playlist->__get('nom') == $_POST['playlist']){
+                            $_SESSION['playlist'] = $playlist;
+                            return $this->form()."<h2>Playlist selected!!</h2>";
+                        }
+                }
             }
         }
         return $this->form();
@@ -58,10 +71,23 @@ class DisplaySavePlaylist extends Action
         $playlists = $rep->findAllUserPlaylists($user['id'], $user['email']);
         $res="<h2>User Playlist</h2><form method='post'>";
         if(empty($playlists)){
-            return "<h2>Vous ne possédez pas encore de Playlists</h2>";
+            $res.="<h2>Vous ne possédez pas encore de Playlists</h2>";
         }
-        foreach($playlists as $playlist){
-            $res.= "<li><button type='submit' name='playlist' value='".$playlist->__get('nom')."'>".$playlist->__get('nom')."</button></li>";
+        else {
+            foreach ($playlists as $playlist) {
+                $res .= "<li><button type='submit' name='playlist' value='" . $playlist->__get('nom') . "'>" . $playlist->__get('nom') . "</button></li>";
+            }
+        }
+
+        $playlists = $rep->findAllPlaylistShared($user['id'], $user['email']);
+        $res.='<h2>User shared Playlist</h2>';
+        if(empty($playlists)){
+            $res.="<h2>Vous ne possédez pas encore de Playlists partagees avec vous</h2>";
+        }
+        else{
+            foreach($playlists as $playlist){
+                $res.= "<li><button type='submit' name='playlist' value='".$playlist->__get('nom')."'>".$playlist->__get('nom')."</button></li>";
+            }
         }
         return $res."</form>";
     }
